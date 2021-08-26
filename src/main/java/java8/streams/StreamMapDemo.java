93c75337628a9ac8a8db4b1efedf221af56860ee
Elena Piaggio <elenapiaggio@gmail.com>
2021-08-26 14:13:34 +0200

    ADD stream with map

--- src/main/java/java8/streams/StreamMapDemo.java
+++ src/main/java/java8/streams/StreamMapDemo.java
@@ -1,2 +1,39 @@
-package java8.streams;public class StreamDemo {
+package java8.streams;
+
+import java.sql.SQLOutput;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class StreamMapDemo {
+
+    public static void main(String[] args) {
+
+        Stream<String> names = Stream
+                .of("Orange", "Strawberry", "Graph", "Lemmon")
+                .peek(fruit -> System.out.println(fruit))
+                .map(name -> {
+                    return name.toUpperCase();
+                });
+
+        names.forEach(System.out::println);
+
+        // Using Reference Methods
+        System.out.println("\n**************** Using Reference Methods ********************");
+        Stream<String> fruits = Stream
+                .of("Apple", "Pineaple", "Watermelon", "Banana")
+                .peek(System.out::println)
+                .map(String::toUpperCase);
+
+        fruits.forEach(System.out::println);
+
+        // Using List
+        System.out.println("\n******************** THE FRUITS *******************");
+        Stream<String> theFruits = Stream
+                .of("Apple", "Pineaple", "Watermelon", "Banana", "Orange", "Strawberry", "Graph", "Lemmon")
+                //.peek(System.out::println)
+                .map(String::toUpperCase);
+        List<String> stringList = theFruits.collect(Collectors.toList());
+        stringList.forEach(System.out::println);
+    }
 }
